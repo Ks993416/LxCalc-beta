467639221c05d6b06e80400f1ae6ae3d20b8fb36
Илья - Ilya <ivailuaoff@gmail.com>
2026-08-27 21:45:35 +0300

    Create source.java

---
+++ source.java
@@ -0,0 +1,17 @@
+/*
+ * Decompiled with CFR 0.152.
+ */
+public class LxCalc {
+    public static int plus(int n, int n2) {
+        return n + n2;
+    }
+
+    public static int minus(int n, int n2) {
+        return n - n2;
+    }
+
+    public static int multi(int n, int n2) {
+        return n * n2;
+    }
+}
+
